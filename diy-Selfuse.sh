3cf77091b3d5f09f3dccec3b5ab2a0ad7b900d7a
ChangeYuan <59167936+yunxi993@users.noreply.github.com>
2022-08-21 21:50:11 +0800

    Update diy-Selfuse.sh

--- diy-Selfuse.sh
+++ diy-Selfuse.sh
@@ -40,11 +40,11 @@ git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git packa
 # containerd Has验证
 #sed -i 's/PKG_HASH:=.*/PKG_HASH:=skip/g' feeds/packages/utils/containerd/Makefile
 
-sed -i '741 a <tr><td width="33%"><%:Compiler author%></td><td>https://t.me/passwall2</td></tr>' package/lean/autocore/files/arm/index.htm
-sed -i '742 a <tr><td width="33%"><%:Compiler author%></td><td>https://t.me/passwall2</td></tr>' package/lean/autocore/files/x86/index.htm
+sed -i '741a \                <tr><td width="33%"><%:Compiler author%></td><td>https://t.me/passwall2</td></tr>' package/lean/autocore/files/arm/index.htm
+sed -i '742a \                <tr><td width="33%"><%:Compiler author%></td><td>https://t.me/passwall2</td></tr>' package/lean/autocore/files/x86/index.htm
 
-sed -i '4123 a msgid "Compiler author"' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '4124 a msgstr "交流群"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '4123a msgid "Compiler author"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '4124a msgstr "交流群"' feeds/luci/modules/luci-base/po/zh-cn/base.po
 
 #echo '<tr><td width="33%"><%:Compiler author%></td><td>https://t.me/passwall2</td></tr>' >> package/lean/autocore/files/arm/index.htm
 #echo '<tr><td width="33%"><%:Compiler author%></td><td>https://t.me/passwall2</td></tr>' >> package/lean/autocore/files/x86/index.htm
