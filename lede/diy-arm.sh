d7ba2442869e906670dbe4e61f993f570cb8e997
Sil free <59167936+yunxi993@users.noreply.github.com>
2023-03-16 02:09:28 +0800

    Update diy-arm.sh

--- lede/diy-arm.sh
+++ lede/diy-arm.sh
@@ -82,6 +82,6 @@ echo 'iptables -I FORWARD 2 -p tcp --dport 5223 -m conntrack --ctstate ESTABLISH
 /etc/init.d/firewall restart" package/lean/default-settings/files/zzz-default-settings
 
 sed -i '741a\
-                <tr><td width="33%">&#32534;&#35793;&#32773;&#58;&#32;&#83;&#105;&#108;</td><td><a href="https://t.me/passwall2" style="color: red;" target="_blank">&#32676;&#32452;&#38142;&#25509;</a></td></tr>\
-                <tr><td width="33%">&#28304;&#30721;&#58;&#32;&#108;&#101;&#100;&#101;</td><td><a href="https://github.com/coolsnowwolf/lede" style="color: red;" target="_blank">&#28304;&#30721;&#38142;&#25509;</a></td></tr>
+                <tr><td width="33%">&#32534;&#35793;&#32773;&#58;&#32;&#83;&#105;&#108;</td><td><a href="https://t.me/passwall2" style="color: black;" target="_blank">&#32676;&#32452;&#38142;&#25509;</a></td></tr>\
+                <tr><td width="33%">&#28304;&#30721;&#58;&#32;&#108;&#101;&#100;&#101;</td><td><a href="https://github.com/coolsnowwolf/lede" style="color: black;" target="_blank">&#28304;&#30721;&#38142;&#25509;</a></td></tr>
 ' package/lean/autocore/files/arm/index.htm
