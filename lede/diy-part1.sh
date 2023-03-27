17ebde1b8a85af4baec2d517620853a42045ff17
Sil free <59167936+yunxi993@users.noreply.github.com>
2023-03-28 00:03:30 +0800

    diy-part1.sh: delete login passwd.

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+sed -i '34,35d' package/lean/default-settings/files/zzz-default-settings
