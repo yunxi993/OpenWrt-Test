113c3574db20a76e306648d39efaf53d05b234f3
ChangeYuan <59167936+yunxi993@users.noreply.github.com>
2023-10-09 01:38:04 +0800

    Modify clone's var.

--- lede/diy-arm.sh
+++ lede/diy-arm.sh
@@ -44,16 +44,14 @@ sed -i 's/services/system/g' feeds/luci/applications/luci-app-cpufreq/luasrc/con
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # Add additional packages
-git clone -b master --single-branch --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git clone -b main --single-branch --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
-git clone -b main --single-branch --depth=1 https://github.com/yunxi993/openwrt-passwall2.git package/openwrt-passwall2
-git clone -b master --single-branch --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-#git clone -b main --single-branch --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
+git clone --depth=1 https://github.com/yunxi993/openwrt-passwall2.git package/openwrt-passwall2
+git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 rm -rf package/helloworld/v2ray-core
 rm -rf package/helloworld/xray-core
 rm -rf package/helloworld/hysteria
-rm -rf package/helloworld/v2ray-core
-rm -rf package/helloworld/v2ray-plugin
 
 # dockerd去版本验证
 #sed -i 's/^\s*$[(]call\sEnsureVendoredVersion/#&/' feeds/packages/utils/dockerd/Makefile
