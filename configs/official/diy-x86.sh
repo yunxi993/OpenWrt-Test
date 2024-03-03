8b4e36a4050f936475154126f9ed2001d685ce91
Sil <yunxi156837538@gmail.com>
2024-03-04 04:58:44 +0800

    Update

--- configs/official/diy-x86.sh
+++ configs/official/diy-x86.sh
@@ -41,8 +41,8 @@ git clone --depth=1 https://github.com/yunxi993/extra.git package/extra
 # Remove snapshot tags
 sed -i 's,-SNAPSHOT,,g' include/version.mk
 sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
-sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By Sil'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt 23.05 $(date +%Y-%m-%d)'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION='Sil'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt-23.05 $(date +%Y-%m-%d)'/g" package/base-files/files/etc/openwrt_release
 cp -f package/extra/banner/Sil  package/base-files/files/etc/banner
 
 sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
