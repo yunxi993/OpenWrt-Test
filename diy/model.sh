e6ee5bb2ac6bc69d1176ed794c8b43ea8d5ece52
Sil <59167936+yunxi993@users.noreply.github.com>
2024-03-06 06:11:07 +0800

    Add fix flow_offloading PPPoE code.

--- diy/model.sh
+++ diy/model.sh
@@ -10,5 +10,9 @@ else
     echo "Generic_x86" > /tmp/sysinfo/model
 fi
 
+sleep 60
+nft flowtable inet fw4 ft-bridges { hook ingress priority filter\; devices = { pppoe-wan, br-lan }\;}
+nft insert rule inet fw4 forward meta l4proto { tcp, udp } flow add @ft-bridges
+
 exit 0
 '> ./package/base-files/files/etc/rc.local
