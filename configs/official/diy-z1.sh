064106434461de3eebc8811794c04191b67c4e52
Sil <59167936+yunxi993@users.noreply.github.com>
2024-03-07 18:30:52 +0800

    Adjust dnsmasq version command priority to x86.

--- configs/official/diy-z1.sh
+++ configs/official/diy-z1.sh
@@ -6,8 +6,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# Dnsmasq switch to 2.90 version
-sed -i "s/2.89/2.90/g" package/network/services/dnsmasq/Makefile
-sed -i "s/02bd230*/8e50309bd837bfec9649a812e066c09b6988b73d749b7d293c06c57d46a109e4/g" package/network/services/dnsmasq/Makefile
-cp -f $GITHUB_WORKSPACE/diy/patches/200-ubus_dns.patch package/network/services/dnsmasq/patches/200-ubus_dns.patch
