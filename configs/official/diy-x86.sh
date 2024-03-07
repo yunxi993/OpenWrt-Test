de906703a655d52f6802ea4de4210244de0ebdd8
Sil <59167936+yunxi993@users.noreply.github.com>
2024-03-08 03:37:18 +0800

    x86.m: Enable modify sysinfo command.

--- configs/official/diy-x86.sh
+++ configs/official/diy-x86.sh
@@ -79,16 +79,15 @@ cp -f $GITHUB_WORKSPACE/diy/patches/200-ubus_dns.patch package/network/services/
 #curl -fsSL https://raw.githubusercontent.com/yunxi993/OpenWrt-Patch/mast/docerdpatch/dockerd.init > feeds/packages/utils/dockerd/files/dockerd.init
 #curl -fsSL https://raw.githubusercontent.com/yunxi993/OpenWrt-Test/main/diy/Makefile feeds/packages/lang/golang/golang/Makefile
 
-#
-#echo '# Put your custom commands here that should be executed once
-## the system init finished. By default this file does nothing.
-#
-#grep "Default string" /tmp/sysinfo/model >> /dev/null
-#if [ $? -ne 0 ];then
-#    echo should be fine
-#else
-#    echo "Generic_x86" > /tmp/sysinfo/model
-#fi
-#
-#exit 0
-#'> ./package/base-files/files/etc/rc.local
+echo '# Put your custom commands here that should be executed once
+# the system init finished. By default this file does nothing.
+
+grep "Default string" /tmp/sysinfo/model >> /dev/null
+if [ $? -ne 0 ];then
+    echo should be fine
+else
+    echo "Generic_x86" > /tmp/sysinfo/model
+fi
+
+exit 0
+'> ./package/base-files/files/etc/rc.local
