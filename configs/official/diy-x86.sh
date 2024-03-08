5abce8cebcee7043a81f4747dbfe347279791e85
Sil <59167936+yunxi993@users.noreply.github.com>
2024-03-08 20:13:02 +0800

    x86.m: sing-box: Some modify.

--- configs/official/diy-x86.sh
+++ configs/official/diy-x86.sh
@@ -29,6 +29,10 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git p
 git clone --depth=1 https://github.com/yunxi993/openwrt-passwall2.git package/openwrt-passwall2
 git clone --depth=1 https://github.com/yunxi993/extra.git package/extra
 
+#sing-box
+cp -f $GITHUB_WORKSPACE/diy/singbox/files package/openwrt-passwall-packages/sing-box/
+sed -i 's,config.json.example,config.json,g' package/openwrt-passwall-packages/sing-box/Makefile
+
 # Update Go Version
 rm -rf feeds/packages/lang/golang && git clone -b 22.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
