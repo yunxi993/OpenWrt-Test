82b6979414093060ac8284dfafd532489b888808
Sil <59167936+yunxi993@users.noreply.github.com>
2024-06-14 04:29:03 +0800

    x86: main: Disable "xray, sing-box" rm commands.

--- configs/official/diy-x86.sh
+++ configs/official/diy-x86.sh
@@ -14,8 +14,8 @@ sed -i 's,OpenWrt,N100,g' package/base-files/files/bin/config_generate
 #sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 # Add additional packages
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/net/sing-box
+#rm -rf feeds/packages/net/xray-core
+#rm -rf feeds/packages/net/sing-box
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
 git clone --depth=1 https://github.com/yunxi993/openwrt-passwall2.git package/openwrt-passwall2
 git clone --depth=1 https://github.com/yunxi993/extra.git package/extra
