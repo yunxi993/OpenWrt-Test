128538d5dbcba21a76540487e93e4f93776942b5
Sil <59167936+yunxi993@users.noreply.github.com>
2024-06-21 09:18:57 +0800

    x86: 'wan' interface default protocol adjust to 'eth0, pppoe'.

--- configs/official/diy-x86.sh
+++ configs/official/diy-x86.sh
@@ -51,6 +51,8 @@ uci add_list network.@device[0].ports='eth1'\n\
 uci add_list network.@device[0].ports='eth2'\n\
 uci add_list network.@device[0].ports='eth3'\n\
 #uci del network.wan\n\
+uci set network.wan.device='eth0'\n\
+uci set network.wan.proto='pppoe'\n\
 uci del network.wan6\n\
 uci commit network\n\n\
 /etc/init.d/irqbalance disable\n\
