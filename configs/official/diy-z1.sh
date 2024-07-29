810f7a256258419e2a4ee8a76f1c19ed7d455ad4
Sil <59167936+yunxi993@users.noreply.github.com>
2024-07-30 01:18:18 +0800

    x86: Add disable `EEE` patch and command.

--- configs/official/diy-z1.sh
+++ configs/official/diy-z1.sh
@@ -6,3 +6,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+cp -rf $GITHUB_WORKSPACE/diy/disable-eee/996-intel-igc-i225-i226-disable-eee.patch target/linux/x86/patches-6.6/
