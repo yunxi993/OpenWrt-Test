2a6a37c3c0d13112c023bd492af162dfdb0fe762
Sil <59167936+yunxi993@users.noreply.github.com>
2024-10-08 01:21:01 +0800

    x86.Z1: Fix command error.

--- configs/official/diy-z1.sh
+++ configs/official/diy-z1.sh
@@ -12,7 +12,8 @@ cp -f $GITHUB_WORKSPACE/diy/config-6.6 target/linux/x86/
 #cat target/linux/x86/config-6.6
 
 sed -i 's,LINUX_VERSION-6.6 = .54,LINUX_VERSION-6.6 = .52,g' include/kernel-6.6
-sed -i 's,6.6.54 = ^5fae86.*,6.6.52 = 1591ab348399d4aa53121158525056a69c8cf0fe0e90935b0095e9a58e37b4b8,g' include/kernel-6.6
+sed -i 's,6.6.54 = 5fae86.*,6.6.52 = 1591ab348399d4aa53121158525056a69c8cf0fe0e90935b0095e9a58e37b4b8,g' include/kernel-6.6
+#sed -i 's,6.6.54 = ^5fae86.*,6.6.52 = 1591ab348399d4aa53121158525056a69c8cf0fe0e90935b0095e9a58e37b4b8,g' include/kernel-6.6
 cat include/kernel-6.6
 
 #ls target/linux/generic/hack-6.6/
