3b51f04fc210b94c93b84db0c5e182f4e8c61d65
Sil <59167936+yunxi993@users.noreply.github.com>
2026-03-30 03:08:24 +0800

    x86: Fix Prepare artifact code, Add kernel patches codes.

--- configs/official/diy-z1.sh
+++ configs/official/diy-z1.sh
@@ -2,7 +2,9 @@
 #
 
 cp -f $GITHUB_WORKSPACE/diy/disable-eee/996-intel-igc-i225-i226-disable-eee.patch target/linux/x86/patches-6.12/
+cp -f $GITHUB_WORKSPACE/diy/disable-eee/996-intel-igc-i225-i226-disable-eee.patch target/linux/x86/patches-6.18/
 find target/linux/x86/patches-6.12/ -name "996-intel-igc-i225-i226-disable-eee.patch"
+find target/linux/x86/patches-6.18/ -name "996-intel-igc-i225-i226-disable-eee.patch"
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
