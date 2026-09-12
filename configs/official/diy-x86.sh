ae153ef4ecf950bc5486e7020da297f46430fe1c
Sil <59167936+yunxi993@users.noreply.github.com>
2026-09-13 03:06:55 +0800

    x86: Adjust local delete commands.

--- configs/official/diy-x86.sh
+++ configs/official/diy-x86.sh
@@ -83,6 +83,7 @@ else
     echo "Generic x86_64" > /tmp/sysinfo/model
 fi
 
+# BEGIN ONCE
 #{ sleep 15; ethtool -A eth0 autoneg off rx on tx on; ethtool -A eth1 autoneg off rx on tx on; } &
 
 (
@@ -111,7 +112,9 @@ fi
 
 ) &
 
-sed -i '/^#{/,/^sed/d' /etc/rc.local && sed -i "/^$/N;/^\n$/D" /etc/rc.local
+#sed -i '/^#{/,/^sed/d' /etc/rc.local && sed -i "/^$/N;/^\n$/D" /etc/rc.local
+sed -i '/^# BEGIN ONCE$/,/^# END ONCE$/d' /etc/rc.local && sed -i "/^$/N;/^\n$/D" /etc/rc.local
+# END ONCE
 
 exit 0
 '> ./package/base-files/files/etc/rc.local
